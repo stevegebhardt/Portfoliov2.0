8bf829eef7f77e4eb4e7f8bd50b4661704824b78
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-05-25 17:37:07 -0400

    somewhat sticky header added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sticky, setSticky] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, []);
+
+  const isSticky = () => {
+    const scrollTop = window.scrollY;
+    const stickyClass = scrollTop >= 250 ? "is-sticky" : "";
+    setSticky(stickyClass);
+    console.log(stickyClass);
+  };
+
+  const classes = `header ${sticky}`;
+
   return (
-    <div>
-      <section>
-        <header>
-          <div>
-            <p>Home</p>
-          </div>
-          <div>
-            <p>Projects</p>
-          </div>
-          <div>
-            <p>About Me</p>
-          </div>
-          <div>
-            <a href="https://github.com/stevegebhardt">My GitHub</a>
-          </div>
-          <div>
-            <a href="https://www.linkedin.com/in/steve-gebhardt-dev/">
-              My LinkedIn
-            </a>
-          </div>
-        </header>
-      </section>
+    <div className="mainSection">
+      <header classsName="header">
+        <div className={classes} id="pageHeader">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#about">About Me</a>
+            </li>
+            <li>
+              <a href="https://github.com/stevegebhardt">My GitHub</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/steve-gebhardt-dev/">
+                My LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <section id="home"></section>
       <section id="projects">
         <div id="project1">
           <div>
             <img></img>
           </div>
           <div>
-            <p></p>
+            <p>Lorem Ipsum</p>
           </div>
         </div>
       </section>
@@ -40,7 +62,7 @@ function App() {
           <img></img>
         </div>
         <div>
-          <p></p>
+          <p>More Lorem Ipsum</p>
         </div>
       </section>
     </div>
